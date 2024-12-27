240e3e82e34c62c70caa87d5e9c01838c033ea2b
StudentBI17 <StudentBI17@SYN13ASS-NB-545.alabuga.local>
2024-12-27 13:58:15 +0300

    Созданна модель для товаров

--- config/magazine/models.py
+++ config/magazine/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Название товара")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
+    img = models.ImageField(upload_to='products/', verbose_name="Изображение", null=True, blank=True)
+    description = models.TextField(verbose_name="Описание")
+
+    def __str__(self):
+        return self.title
