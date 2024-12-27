92d6974c660bd4904a972ec84e61661e7a19391d
StudentBI17 <StudentBI17@SYN13ASS-NB-545.alabuga.local>
2024-12-27 14:54:18 +0300

    сделал urls

--- config/magazine/urls.py
+++ config/magazine/urls.py
@@ -1,7 +1,25 @@
 from django.urls import path
-from .views import ProductsListView
-
+from .views import (
+    ProductsListView,
+    ProductsCreateView,
+    ProductsUpdateView,
+    ProductsDeleteView,
+    ProductsDetailsView,
+)
 
 urlpatterns = [
-    path('', ProductsListView.as_view(), name='index')
+    # Список товаров
+    path('goods/', ProductsListView.as_view(), name='products_list'),
+
+    # Детали товара
+    path('goods/<int:pk>/', ProductsDetailsView.as_view(), name='product_detail'),
+
+    # Создание товара
+    path('goods/create/', ProductsCreateView.as_view(), name='product_create'),
+
+    # Редактирование товара
+    path('goods/<int:pk>/update/', ProductsUpdateView.as_view(), name='product_update'),
+
+    # Удаление товара
+    path('goods/<int:pk>/delete/', ProductsDeleteView.as_view(), name='product_delete'),
 ]
