4056d9fd786067f51c093337ff6638b792ec16b8
StudentBI17 <StudentBI17@SYN13ASS-NB-545.alabuga.local>
2024-12-27 14:58:08 +0300

    fix

--- config/magazine/urls.py
+++ config/magazine/urls.py
@@ -8,18 +8,9 @@ from .views import (
 )
 
 urlpatterns = [
-    # Список товаров
     path('goods/', ProductsListView.as_view(), name='products_list'),
-
-    # Детали товара
     path('goods/<int:pk>/', ProductsDetailsView.as_view(), name='product_detail'),
-
-    # Создание товара
     path('goods/create/', ProductsCreateView.as_view(), name='product_create'),
-
-    # Редактирование товара
     path('goods/<int:pk>/update/', ProductsUpdateView.as_view(), name='product_update'),
-
-    # Удаление товара
     path('goods/<int:pk>/delete/', ProductsDeleteView.as_view(), name='product_delete'),
 ]
